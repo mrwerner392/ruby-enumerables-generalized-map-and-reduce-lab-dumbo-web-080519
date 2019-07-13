80a58a2c3f4d8a684cfa48f5e320b24802572603
Matt Werner <mrwerner392@gmail.com>
2019-07-13 18:52:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def my_own_map(array)
+  counter = 0
+  new_array = []
+
+  while counter < array.length do
+    new_array.push(yield(array[counter]))
+    counter += 1
+  end
+
+  new_array
+end

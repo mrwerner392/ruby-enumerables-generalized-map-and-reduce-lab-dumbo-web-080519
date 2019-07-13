09e871777305894afd9233db5279db3793c20c0b
Matt Werner <mrwerner392@gmail.com>
2019-07-13 19:04:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
-# Your Code Here
+# maps array elements onto a new array
 def map(array)
   counter = 0
   new_array = []
@@ -9,4 +9,15 @@ def map(array)
   end
 
   new_array
+end
+
+def reduce(array, reduce_value = 0)
+  counter = 0
+
+  while counter < array.length do
+    yield(array[counter])
+    counter += 1
+  end
+
+  reduce_value
 end
